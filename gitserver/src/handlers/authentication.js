d1517c6c7fefe9cfbee26bdd524e00e018722d6d
Jacob McSwain <jacob@clevyr.com>
2019-05-25 01:23:50 -0500

    Gitserver: Fixup authentication

--- gitserver/src/handlers/authentication.js
+++ gitserver/src/handlers/authentication.js
@@ -1,10 +1,20 @@
-module.exports = (type, repo, user, next) => {
-    if (type == 'push') {
+const db = require('../db/models');
+const argon2 = require('argon2');
+
+module.exports = ({ type, repo, user }, next) => {
+    return new Promise((resolve, reject) => {
         user((username, password) => {
-            console.log(username, password);
-            next();
+            db.users.findOne({
+                where: { login: username },
+            }).then((foundUser) => {
+                argon2.verify(foundUser.gitPassword, password).then((verified) => {
+                    verified ? resolve() : reject(new Error('Invalid Username or Password'));
+                }).catch((err) => {
+                    reject(err);
+                });
+            }).catch((err) => {
+                reject(err);
+            });
         });
-    } else {
-        next();
-    }
+    });
 };
