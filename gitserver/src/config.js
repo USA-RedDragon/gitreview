cfad3a163a8c26baea0b762061f9cf44317a553d
Jacob McSwain <jacob@clevyr.com>
2019-05-25 01:22:50 -0500

    Gitserver: Fixup config

--- gitserver/src/config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    repoPath: process.env.REPO_PATH || path.resolve(__dirname, '../repos'),
-    autoCreate: Boolean(process.env.AUTO_CREATE_REPOS) || true,
-    port: process.env.PORT || 29418,
-};
