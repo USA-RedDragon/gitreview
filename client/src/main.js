220e0d23cf73096cfa0edf333c4135c4519c0501
Jacob McSwain <jacob@clevyr.com>
2019-05-25 01:19:31 -0500

    Client: remove useless comment import

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import router from './router'
 
-// require('./components/bootstrap')
-
 Vue.config.productionTip = false
 
 import App from './App.vue'
